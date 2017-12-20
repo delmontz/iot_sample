c351e0132145ae6aeec9d884eec87b911723378d
delmont <pazxx219@gmail.com>
2017-12-21 01:02:27 +0900

    登録:LINEへ通知サンプルコード

---
+++ LINEへ通知/send_line/send_line.ino
@@ -0,0 +1,41 @@
+#include <ESP8266WiFi.h>
+
+WiFiClientSecure clientSecure;
+
+char ssid[] = "家のSSID";
+char pass[] = "ネットのパスワード";
+String mes = "LINEへの通知メッセージ";
+String token = "アクセストークン";
+
+void setup() {
+  Serial.begin(74880);
+  //ネットへ接続
+  WiFi.mode(WIFI_STA);
+  WiFi.begin(ssid, pass);
+  while (WiFi.status() != WL_CONNECTED) {
+    Serial.print(".");
+    delay(1000);
+  }
+  Serial.println("connected!");
+  
+  //ここからLINEへの送信メッセージ作成
+  clientSecure.connect("notify-api.line.me", 443);
+  String body = "message=" + mes;
+  String req = "";
+  req += "POST /api/notify HTTP/1.1\r\n";
+  req += "Host: notify-api.line.me\r\n";
+  req += "Authorization: Bearer " + token + "\r\n";
+  req += "Cache-Control: no-cache\r\n";
+  req += "User-Agent: ESP8266\r\n";
+  req += "Content-Type: application/x-www-form-urlencoded\r\n";
+  req += "Content-Length: " + String(body.length()) + "\r\n";
+  req += "\r\n";
+  req += body;
+  //ここまで
+  clientSecure.print(req);  //送信実行
+  Serial.println("送信しました！");
+}
+
+void loop() {
+  /* 何もしない */
+}
