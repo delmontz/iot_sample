d80f96ad26748e3b5b965c64f32343ce4b599fc8
Kouyu Takanuma <pazxx219@gmail.com>
2018-06-16 09:40:54 +0900

    引数逆、全角混じってる・・・

--- LEDチカチカ/lchika/lchika.ino
+++ LEDチカチカ/lchika/lchika.ino
@@ -1,6 +1,6 @@
 void setup() {
     //IO14ピンを出力に設定する
-    pinMode(OUTPUT,14);
+    pinMode(14,OUTPUT);
 }
     
 void loop() {
@@ -10,6 +10,6 @@ void loop() {
     delay(1000);
     //LEDを消す
     digitalWrite(14,LOW);
-　  //一秒待つ
+    //一秒待つ
     delay(1000);
 }
